766796e9b4b4d971cef349b31c1e43d7e18e7639
dshutnalov <dshutnalov@gmail.com>
2017-11-27 20:36:40 +0200

    Calculation

--- app/src/main/java/com/example/alex/fifatrader/calculation.java
+++ app/src/main/java/com/example/alex/fifatrader/calculation.java
@@ -2,6 +2,7 @@ package com.example.alex.fifatrader;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.renderscript.Double2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,7 +14,11 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import static android.widget.Toast.LENGTH_LONG;
+
 public class calculation extends Activity {
+    double finalProfit = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +33,8 @@ public class calculation extends Activity {
         final EditText buyingPrice = (EditText) findViewById(R.id.bought);
         final EditText sellingPrice = (EditText) findViewById(R.id.sold);
         Button confirm = (Button) findViewById(R.id.confirm);
+        final SharedPreferences prefs = getSharedPreferences("name", MODE_PRIVATE);
+        final SharedPreferences.Editor editor = getSharedPreferences("name", MODE_PRIVATE).edit();
 
 
 
@@ -38,16 +45,29 @@ public class calculation extends Activity {
                     Toast.makeText(calculation.this, "This place can not be empty", Toast.LENGTH_SHORT).show();
                 }
                 else{
-                    double price = Double.parseDouble(buyingPrice.getText().toString());
-                    double selling = Double.parseDouble(sellingPrice.getText().toString());
-                    double temp = selling * 5 / 100;
-                    double profit = (selling - temp) - price;
-                    String profitstring = Double.toString(profit);
-                    String taxstring = Double.toString(temp);
-                    Intent intent = new Intent(calculation.this, tradeSession.class);
-                    intent.putExtra("profit",profitstring);
-                    intent.putExtra("tax", taxstring);
-                    startActivity(intent);
+                        double price = Double.parseDouble(buyingPrice.getText().toString());
+                        double selling = Double.parseDouble(sellingPrice.getText().toString());
+                        double temp = selling * 5 / 100;
+                        double profit = (selling - temp) - price;
+
+                        finalProfit = Double.parseDouble(prefs.getString("finalProfit",null)) + Double.parseDouble(prefs.getString("profit", null));
+                        editor.putString("profit", Double.toString(profit));
+                        editor.putString("finalProfit", Double.toString(finalProfit));
+                        editor.commit();
+
+
+                        String profitstring = prefs.getString("finalProfit", null);
+                        String taxstring = Double.toString(temp);
+
+                        Intent intent = new Intent(calculation.this, tradeSession.class);
+                        intent.putExtra("profit", profitstring);
+                        intent.putExtra("tax", taxstring);
+
+                        String profitCalculation = prefs.getString("finalProfit", null);
+                        startActivity(intent);
+                        Toast toast = Toast.makeText(calculation.this, profitCalculation, LENGTH_LONG);
+                        toast.show();
+
                 }
 
             }
