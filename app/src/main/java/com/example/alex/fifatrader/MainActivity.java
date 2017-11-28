0259237d9481ad227f72e6eb2c37029bc1a26c1a
alexander126 <akalapsazov@yahoo.com>
2017-11-28 15:09:14 +0000

    activitybar update

--- app/src/main/java/com/example/alex/fifatrader/MainActivity.java
+++ app/src/main/java/com/example/alex/fifatrader/MainActivity.java
@@ -162,18 +162,16 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.trade_session) {
+            Intent intent = new Intent(MainActivity.this,tradeSession.class);
+            startActivity(intent);
+        } else if (id == R.id.calculator) {
+            Intent intent = new Intent(MainActivity.this,calculator.class);
+            startActivity(intent);
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.taxeplain){
+            Intent intent = new Intent(MainActivity.this,TaxExplained.class);
+            startActivity(intent);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
